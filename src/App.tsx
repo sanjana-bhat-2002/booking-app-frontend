9bbc875e10f063c494546ee2007164e9906e00d2
sanjana-bhat-2002 <asanjanabbhat@gmail.com>
2024-07-22 22:43:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/sections/Hero'
+import MainLayout from './layouts/MainLayout'
+
+function App() {
+  return (
+    <>
+      <MainLayout>
+        <Hero/>
+      </MainLayout>
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import React, { FC, ReactNode } from 'react'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+interface LayoutProps {
+    children: ReactNode
+}
+
+const MainLayout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className='mx-6'> 
+    <Header />
+     {children} 
+     <Footer />
+    </div>
+  )
+}
+
+export default MainLayout;
